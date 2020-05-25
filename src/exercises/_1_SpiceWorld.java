44910ff32cbe9c899be2896509a99d033f57a667
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-05-25 00:57:23 -0700

    im not sure i completed properly

--- src/exercises/_1_SpiceWorld.java
+++ src/exercises/_1_SpiceWorld.java
@@ -6,18 +6,17 @@ public class _1_SpiceWorld {
 	 */
 
 	public static void main(String[] args) {
-		SpiceGirl sporty = new SpiceGirl() {
-			
-			@Override
-			public void perform() {
-				System.out.println("Do backflips.");
-				
-			}
-		};
-		sporty.perform();
-	}
-}
+		SpiceGirl sporty = new SpiceGirl();
+		
+		@Override	
+		sporty.perform(() -> System.out.println("Do backflips.")};
+		
+		
+	
+
 
 interface SpiceGirl {
 	void perform();
 }
+}
+
